e06703b336d706131a708640c15567065d469f88
akshaypermunda <71223550+akshaypermunda@users.noreply.github.com>
2020-09-13 18:25:45 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A dashboard which  provides an interactive visualization of COVID-19 Data which can be customized accroding to local interests.',
+    author='Akshay Permunda',
+    license='',
+)
